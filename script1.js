530f43a7115f101317fd7678d1079fbc80c71cbd
SyedZeeshanHussain <47963919+SyedZeeshanHussain@users.noreply.github.com>
2019-09-28 11:37:52 +0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,16 @@
+// 1. Write a script to greet your website visitor using JS alert box.
+alert("Welcome to our website!");
+
+// 2. Write a script to display following message on your web page:
+alert("Error! Please enter a valid password.");
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break)
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// 4. Write a script to display following messages in sequence:
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// 5. Generate the following message through browser’s developer console:
+alert("Hello... I can run JS through my web browser's console");
+
